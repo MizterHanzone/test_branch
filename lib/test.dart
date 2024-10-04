0554f7d235df9de352d23838a0024f5265221b00
MizterHanzone <155834736+MizterHanzone@users.noreply.github.com>
2024-10-04 20:46:48 +0700

    addTest

---
+++ lib/test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Widgetdialog extends StatelessWidget {
+  const Widgetdialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) => const AboutDialog(
+                      applicationIcon: FlutterLogo(),
+                      applicationLegalese: 'Legalese',
+                      applicationName: 'Flutter App',
+                      applicationVersion: '3.22',
+                      children: [
+                        Text("This is create by flutter"),
+                      ],
+                    ));
+          },
+          child: const Text("Show dialog")),
+    );
+  }
+}
